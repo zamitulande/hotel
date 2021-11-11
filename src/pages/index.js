3ec7d09e350a90830336d9d225f4b5b894ba04b0
zami tulande <hulmartulande@gmail.com>
2021-11-11 16:50:05 -0500

    se agregan rutas de estilo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 
 const IndexPage = () => {
   return (
-   <h1>hola</h1>
+    <Layout>
+        <h1>Gatsby Hotel</h1>
+    </Layout>
+   
   )
 }
 
